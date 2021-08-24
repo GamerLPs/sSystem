858d41a1de910a283a462d46b66341fd5edf3d9b
GamerLPs <andre_alscher98@yahoo.de>
2021-08-24 20:38:12 +0200

    Vault beigefügt.

--- src/main/java/de/Barryonixx/Main.java
+++ src/main/java/de/Barryonixx/Main.java
@@ -15,17 +15,7 @@ public final class Main extends JavaPlugin {
         getCommand("wetter").setExecutor(new WETTERCOMMAND());
         getCommand("gamemode").setExecutor(new GAMEMODECOMMAND());
 
-        /*
-        * So jetzt... sagen wir du codest jetzt hier den Commentar raus...
-        * Wenn du fertig bist, gehst du oben rechts auf den grünen hacken (Commit)
-        *
-        * Dann oben links alle Datein auswählen...also mit dem hacken
-        * Dann da wo jetzt Commit message steht, kannst du schreiben was du gemacht hast...Am besten kurz
-        * Und dann einfach auf Commit and Push
-        * Wenn es fehler gibt, liegt das daran, weil intellij sagt, das der Javacode nicht gut aussieht...
-        * einfach auf Commit und Push nochmal klicken und dann auf Commit  & Push anyway
-        *
-        * */
+        //
 
 
         //LISTENERS
