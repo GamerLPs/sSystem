a9b54e7c88b7754444f4641d3a35a5a00ad6dcb0
TheWebcode <89354835+TheWebcode@users.noreply.github.com>
2021-08-26 16:51:01 +0200

    Vaultmanager hinzugefügt

--- src/main/java/de/Barryonixx/Main.java
+++ src/main/java/de/Barryonixx/Main.java
@@ -2,16 +2,20 @@ package de.Barryonixx;
 
 import de.Barryonixx.COMMANDS.GAMEMODECOMMAND;
 import de.Barryonixx.COMMANDS.WETTERCOMMAND;
+import de.Barryonixx.vault.Vaultmanager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
+    private Vaultmanager vaultmanager;
     public static String perm = "§c<§4§l✘§c> §7Keine Permission §c<§4§l✘§c>";
 
     @Override
     public void onEnable() {
         //COMMANDS
+        this.vaultmanager = new Vaultmanager();
+
         getCommand("wetter").setExecutor(new WETTERCOMMAND());
         getCommand("gamemode").setExecutor(new GAMEMODECOMMAND());
 
